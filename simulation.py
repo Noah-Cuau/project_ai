fcfc4bc9ae65c95b2d22f76f94ac7c189f793630
Noah Cuau <noah.cuau@etu.u-bordeaux.fr>
2026-02-10 14:54:06 +0100

    debut boule

---
+++ simulation.py
@@ -0,0 +1,17 @@
+
+
+class Boules:
+    def __init__(self, x, y, angle, eyes_list, pilot):
+        self.x = x
+        self.y = y
+        self.angle =angle
+        self.eye_list = eyes_list
+        self.pilot = pilot
+        self.radius = 10
+        self.healt = 3
+
+    def move(self, x_move, y_move, angle):
+        self.x += x_move
+        self.y += y_move
+        self.angle += angle
+    
