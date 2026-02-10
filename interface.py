56221a80ce9edc1d72c2fafad62a69ee2ae986a8
Lucas Diaz <lucas.diaz@etu.u-bordeaux.fr>
2026-02-10 16:45:29 +0100

    interface

--- interface.py
+++ interface.py
@@ -1,6 +1,5 @@
 import pygame
 from simulation import *
-
 class game:
     def __init__(self, largeur, hauteur):
         pygame.init()
@@ -10,27 +9,33 @@ class game:
         self.running = True
         self.param_rect = [self.echelle,self.echelle,3* self.echelle, 3 * self.echelle, 5]
         self.rect = pygame.Rect(self.param_rect[0],self.param_rect[1],self.param_rect[2],self.param_rect[3])
+    def quadrillage(self):
+        for row in range(self.param_rect[3] // self.echelle):
+            for col in range(self.param_rect[2] // self.echelle):
+
+                x = self.param_rect[0] + col * self.echelle
+                y = self.param_rect[1] + row * self.echelle
+
+                rect = pygame.Rect(x, y, self.echelle, self.echelle)
+
+                if (row + col) % 2 == 0:
+                    color = "lightgrey"
+                else:
+                    color = "darkgrey"
+
+                pygame.draw.rect(self.screen, color, rect)
 
     def run(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-        self.screen.fill("white")
-        pygame.draw.rect(self.screen,"black",self.rect,self.param_rect[4])
-        self.quadrillage()
-        # render simultation
-        pygame.display.flip()
-
-        self.clock.tick(60)  
-    def quadrillage(self):
-        for i in range(self.echelle+(self.param_rect[0]+self.param_rect[4]), self.param_rect[0]+self.param_rect[2]+self.param_rect[4], self.echelle):
-            pygame.draw.line(self.screen,"grey",(i,self.param_rect[1]),(i,self.param_rect[1]+self.param_rect[3]),2)
-        for i in range(self.echelle+ self.param_rect[1]+self.param_rect[4], self.param_rect[1]+self.param_rect[3]+self.param_rect[4], self.echelle):
-            pygame.draw.line(self.screen,"grey",(self.param_rect[0],i),(self.param_rect[0]+self.param_rect[2],i),2)
-
-
-if __name__ == "__main__":
-    new_game = game(5,5)
+            self.screen.fill("white")
+            pygame.draw.rect(self.screen,"black",self.rect,self.param_rect[4])
+            self.quadrillage()# render simultation
+            pygame.display.flip()
+            self.clock.tick(60)
+    
+if __name__ == "__main__":                                
+    new_game = game(3,3)
     while(new_game.running):
         new_game.run()
-
