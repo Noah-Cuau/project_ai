13dc31d2ea974f79caabf1f970257a688ec54559
Noah Cuau <noah.cuau@etu.u-bordeaux.fr>
2026-02-10 16:15:00 +0100

    first attemp for sim

--- simulation.py
+++ simulation.py
@@ -8,18 +8,14 @@ class Boule:
         self.eye_list = eyes_list
         self.pilot = pilot
         self.radius = 10
-        self.healt = 3
+        self.health = 3
 
     def move(self, x_move, y_move, angle):
         self.x += x_move
         self.y += y_move
         self.angle += angle
     
-class board:
-    def __init__(self):
-        self.boules = []
-        self.spikes = []
-        self.wall = []
+
     
 
 class Spike:
@@ -27,6 +23,7 @@ class Spike:
         self.x = x
         self.y = y
         self.pilot = pilot
+        self.radius =10
 
     def move(self):
         self.pilot.move(self)
@@ -56,7 +53,43 @@ class Spike_Pilot:
         spike.x += self.right
         spike.y += self.up
 
-      
+class Board:
+    def __init__(self, width, height):
+        self.boules = []
+        self.spikes = []
+
+    def run(self):
+        for spike in self.spikes:
+             spike.move()
+
+    def add_spike(self, spike):
+        self.spikes.append(spike)
+
+
+
+def create_sim_test(width, height):
+    new_b = Board(width,height)
+    for i in range(10):
+        new_pilot = Spike_Pilot(width,height)
+        new_spike = Spike(10*i, 10*i,new_pilot)
+        new_b.add_spike(new_spike)
+    return new_b
+
+if __name__ == "__main__":
+    b = create_sim_test(1000,1000)
+    for i in range(1000):
+        b.run()
+        for s in b.spikes:
+            if (s.x >1000) or (s.y >1000) or s.y<0 or s.x <0:
+                print("error at frame "+str(i))
+        
+    
+    
+     
+    
+        
+
+
         
 
 
