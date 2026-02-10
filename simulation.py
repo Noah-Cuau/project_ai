94f7d3b276f7d737231eb49ddba9f8ce5ce2bc19
Noah Cuau <noah.cuau@etu.u-bordeaux.fr>
2026-02-10 15:59:16 +0100

    spike

--- simulation.py
+++ simulation.py
@@ -1,6 +1,6 @@
 
 
-class Boules:
+class Boule:
     def __init__(self, x, y, angle, eyes_list, pilot):
         self.x = x
         self.y = y
@@ -15,3 +15,43 @@ class Boules:
         self.y += y_move
         self.angle += angle
     
+
+class Spike:
+    def __init__(self, x, y, pilot):
+        self.x = x
+        self.y = y
+        self.pilot = pilot
+
+    def move(self):
+        self.pilot.move(self)
+
+
+
+class Spike_Pilot:
+    def __init__(self, board_w, board_h):
+            self.board_w = board_w
+            self.board_h = board_h
+            self.up = 1
+            self.right = 1
+    
+    def move(self, spike):
+        if (self.up == 1):
+            if spike.y+1 > self.board_h:
+                self.up = -1
+        elif spike.y-1 <0:
+                self.up =1
+
+        if (self.right == 1):
+            if spike.x+1 > self.board_w:
+                self.right = -1
+        elif spike.x-1 <0:
+                self.right =1
+
+        spike.x += self.right
+        spike.y += self.up
+
+      
+        
+
+
+
